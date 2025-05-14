6e03eae73472de9d3131153287ccc9dcc4c5ce6f
annas <anna.surmacz00@gmail.com>
2025-05-14 19:29:02 +0200

    Zadanie 7.2 niemutowalny użytkownik forum

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.lambda.Processor;
 import com.kodilla.stream.reference.FunctionalCalculator;
@@ -32,6 +33,9 @@ public class StreamMain {
         poemBeautifier.beautify("Przykładowy tekst", (poem) -> poem.toUpperCase());
         poemBeautifier.beautify("Przykładowy tekst", (poem) -> ":)" + poem + ":)");
         poemBeautifier.beautify("Przykładowy tekst", (poem) -> "<<<<<<***>>>>>>" + poem + "<<<<<<***>>>>>>");
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
 
     }
 }
