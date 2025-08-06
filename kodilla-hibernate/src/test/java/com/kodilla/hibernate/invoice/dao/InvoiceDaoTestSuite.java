1baa640da2644b962a7e3543648559f6b00196b1
annas <anna.surmacz00@gmail.com>
2025-08-06 08:29:40 +0200

    zadanie 16.3

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/invoice/dao/InvoiceDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/invoice/dao/InvoiceDaoTestSuite.java
@@ -3,6 +3,7 @@ package com.kodilla.hibernate.invoice.dao;
 import com.kodilla.hibernate.invoice.Invoice;
 import com.kodilla.hibernate.invoice.Item;
 import com.kodilla.hibernate.invoice.Product;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -25,6 +26,7 @@ public class InvoiceDaoTestSuite {
     private ItemDao itemDao;
 
     @Test
+    @Transactional
     void testInvoiceDaoSave() {
         // Given
         Product product1 = new Product("Keyboard");
@@ -52,6 +54,30 @@ public class InvoiceDaoTestSuite {
         // Then
         assertNotEquals(0, invoiceId);
         assertEquals(3, invoice.getItems().size());
+        assertNotEquals(0, invoiceId);
+        Invoice myInvoice = invoiceDao.findById(invoiceId).orElse(null);
+        assertNotNull(myInvoice);
+        assertEquals("FV-001/2025", myInvoice.getNumber());
+
+        Item myItem1 = myInvoice.getItems().get(0);
+        Item myItem2 = myInvoice.getItems().get(1);
+        Item myItem3 = myInvoice.getItems().get(2);
+
+        assertEquals("Keyboard",myItem1.getProduct().getName());
+        assertEquals(new BigDecimal("150.00"), myItem1.getPrice());
+        assertEquals(2, myItem1.getQuantity());
+        assertEquals(new BigDecimal("300.00"), myItem1.getValue());
+
+        assertEquals("Monitor", myItem2.getProduct().getName());
+        assertEquals(new BigDecimal("800.00"), myItem2.getPrice());
+        assertEquals(1, myItem2.getQuantity());
+        assertEquals(new BigDecimal("800.00"), myItem2.getValue());
+
+        assertEquals("Mouse", myItem3.getProduct().getName());
+        assertEquals(new BigDecimal("50.00"), myItem3.getPrice());
+        assertEquals(3, myItem3.getQuantity());
+        assertEquals(new BigDecimal("150.00"), myItem3.getValue());
+
 
         // CleanUp
         invoiceDao.deleteById(invoiceId);
